6ab18c2d63836ee03bf3f5b0b4280056309c4398
Zhai Jinpeng <914962409@qq.com>
2024-08-09 17:05:00 +0800

    added documentation to secant.py

--- ballistics/num/secant.py
+++ ballistics/num/secant.py
@@ -11,8 +11,50 @@ def secant(
     tol: float = 1e-16,
     max_it: int = 100,
 ) -> Tuple[float, float]:
+    """
+    use secant method to solve the zero of a univariate function.
+
+
+
+    Parameters
+    ----------
+    f: Callable[[float], float]
+        univariate function defined on (at least) [x_min, x_max].
+    x_0, x_1: float
+        two initial guesses to seed the secant algorithm.
+    x_min, x_max: float
+        bounds on which the root is to be found. Default values
+        [-inf, +inf].
+    tol: float
+        convergence criteria. The algorithm will terminate when the solution for the
+        root does not vary more than abs(tol) in any single iteration.
+    max_it: int
+        terminating condition, maximum number of iterations before the calculation
+        is aborted.
+
+
+    Raises
+    ------
+    ValuError
+        if the maximum number of iteration is exceeded.
+
+    Notes
+    -----
+
+    The secant method allows faster convergence on well behaved functions,
+    but does not guarantee convergence to a root. it is incumbent upon
+    the user to check that the algorithm did not converge to one of the bounds
+    (`x_min`, `x_max`) even when the algorithm terminates without error.
+
+    When an error is raised, there are a few possibilities:
+    - the function does not have a root on [x_min, x_max], in which case the solution
+    will "jump" around a stationary point without converging.
+    - the function may not be well behaved in proximity to the root.
+
+    """
     tol = abs(tol)
     fx_0, fx_1 = f(x_0), f(x_1)
+    x_min, x_max = min(x_min, x_max), max(x_min, x_max)
 
     if x_0 == x_1 or fx_0 == fx_1:
         raise ValueError(
@@ -35,10 +77,4 @@ def secant(
         x_0, x_1, fx_0, fx_1 = x_1, x_2, fx_1, fx_2
 
     else:
-        raise ValueError(
-            f"Secant method called from {x_min} to {x_max}\n"
-            + f"Maximum iteration exceeded at it = {i}/{max_it}"
-            + ",\n[0] f({})={}->\n[1] f({})={}->\n[2] f({})={}".format(
-                x_0, fx_0, x_1, fx_1, x_2, fx_2
-            )
-        )
+        raise ValueError("Maximum iteration exceeded")
