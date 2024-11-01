d023a5a3954bbbe18f5b71e656c77a84f55fa503
Zhai Jinpeng <914962409@qq.com>
2024-11-01 14:57:26 +0800

    inc

--- res/guns/generate_example_guns.py
+++ res/guns/generate_example_guns.py
@@ -3,8 +3,7 @@ import logging
 from a_19 import a_19
 from ballistics.gun import Gun
 from bs_3 import bs_3
-from zis_2 import zis_2
-from zis_3 import zis_3
+from m_46 import m_46_full, m_46_one
 
 logger = logging.getLogger(__name__)
 
@@ -24,6 +23,8 @@ if __name__ in {"__main__", "__mp_main__"}:
 
     guns.append(bs_3)
     guns.append(a_19)
+    guns.append(m_46_full)
+    guns.append(m_46_one)
 
     Gun.to_file(guns=guns, filename="example_guns.json")
     logger.info("Ended")
